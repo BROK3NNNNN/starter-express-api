44658697c9ff8949b2e3b27e6cd9533a8e68170f
BROK3NNNNN <134335455+BROK3NNNNN@users.noreply.github.com>
2023-05-23 20:54:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require("express")
+const router = express.Router()
+const mongoose = require("mongoose")
+
+
+const diaksema = new mongoose.Schema({
+  name:{
+    type: String,
+  },
+  school:{
+    type: String,
+  },
+  age:{
+    type: String
+  }
 })
-app.listen(process.env.PORT || 3000)
+
+const Diak = mongoose.model("diakok", diaksema)
+
+
+
+
+
+
+router.get("/", async (req, res) =>{
+  const lista = await Diak.find().sort("name")
+  res.send(lista)
+})
+
+
+
+router.post("/", async(req, res) => {
+  var ujdiak = new Diak({
+    school: req.body.school,
+    name: req.body.name,
+    age: req.body.age
+  })
+  ujdiak = await ujdiak.save()
+  res.send(ujdiak)
+})
+
+
+router.put("/:id", async(req, res) =>{
+  const csere = await  Diak.findByIdAndUpdate(req.params.id, {name: req.body.name},{
+    new: true
+  })
+
+  if(!csere){
+    return res.status(400).send("HIBA :(")
+  }
+  res.send(csere)
+
+})
+
+router.delete("/:id", async(req, res) =>{
+  const torol = await Diak.findByIdAndDelete(req.params.id, {name: req.body.name},{
+    new: true
+  })
+  if(!torol){
+    return res.status(400).send("HIBA :(")
+  }
+  res.send(torol)
+})
+
+module.exports = router
